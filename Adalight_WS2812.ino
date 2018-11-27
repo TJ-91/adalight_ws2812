83352d0e8937eaaa42eca95978b0e082b9da7977
TJ-91 <26199554+TJ-91@users.noreply.github.com>
2018-11-27 15:29:35 +0100

    first working version of smooth color transition

--- Adalight_WS2812.ino
+++ Adalight_WS2812.ino
@@ -1,23 +1,124 @@
 /*
- * Arduino interface for the use of WS2812 strip LEDs
- * Uses Adalight protocol and is compatible with Boblight, Prismatik etc...
- * "Magic Word" for synchronisation is 'Ada' followed by LED High, Low and Checksum
- * @author: Wifsimster <wifsimster@gmail.com> 
- * @library: FastLED v3.001
- * @date: 11/22/2015
+ * Project forked from Wifsimster/adalight_ws2812
+ * 
+ * The fork provides the functionality of smooth color transitions
  */
+ 
 #include "FastLED.h"
-#define NUM_LEDS 240
-#define DATA_PIN 6
+#include "CircularBuffer.h"
 
-// Baudrate, higher rate allows faster refresh rate and more LEDs (defined in /etc/boblight.conf)
-#define serialRate 115200
+/* 
+ *  Configure according to your hardware setup
+ */
+#define NUM_LEDS    53
+#define DATA_PIN    5
+#define serialRate  115200 // Baudrate, higher rate allows faster refresh rate and more LEDs (defined in /etc/boblight.conf)
+
+/*
+ * Configure the smooth transition settings
+ */
+#define SMOOTH_TRANSITION_STEPS             32    // number of intermediate colors
+#define SMOOTH_TRANSITION_STEP_SIZE         8     // how big each step of intermediate colors is. Recommended: SMOOTH_TRANSITION_STEPS * SMOOTH_TRANSITION_STEP_SIZE > 255
+#define SMOOTH_TRANSITION_WAIT_INTERMEDIATE 3000  // wait that many µs after each intermediate color. Should be a mutliple of 100.
+
+/* 
+ * Constants
+ */
+#define FRAME_SIZE (3*NUM_LEDS + 6) // 3 byte per LED + 6 bytes header; can hold up to one complete frame
 
 // Adalight sends a "Magic Word" (defined in /etc/boblight.conf) before sending the pixel data
 uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;
 
-// Initialise LED-array
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];                            // current LED color to be displayed
+CRGB leds_target_color[NUM_LEDS];               // target color for smoothing
+CircularBuffer<uint8_t, FRAME_SIZE> read_buf;   // stores up to one frame of data
+
+
+void delay_microseconds_and_read(int microsecs)
+{
+  while(microsecs)
+  {
+    fill_read_buf();
+    delayMicroseconds(100);
+    microsecs -= 100;
+  }
+}
+
+/*
+ * Read one byte from our read_buf
+ * if needed, read one byte from serial
+ */
+uint8_t read_byte()
+{
+  int read_val = -1;
+  if(read_buf.isEmpty())
+  {
+    while(read_val == -1)
+    {
+      read_val = Serial.read();
+    }
+    read_buf.push(read_val);
+  }
+  
+  return read_buf.shift();
+}
+
+void fill_read_buf()
+{
+  while(Serial.available())
+  {
+    read_buf.push(Serial.read());
+  }
+}
+
+int smooth_step(int from, int to, int step_size)
+{
+  int result; 
+  
+  if(from == to)
+  {
+    return to;
+  }
+  
+  if(from > to)
+  {
+    result = from - step_size;
+    if(result < to)
+    {
+      return to;
+    }
+  }
+  else
+  {
+    result = from + step_size;
+    if(result > to)
+    {
+      return to;
+    }
+  }
+
+  return result;
+}
+
+void show_colors_smooth()
+{
+  int intermediate_colors_wait_time_micros = SMOOTH_TRANSITION_WAIT_INTERMEDIATE; 
+  int intermediate_colors_step_size = SMOOTH_TRANSITION_STEP_SIZE;
+  int steps = SMOOTH_TRANSITION_STEPS;
+  while(steps--)
+  {
+    for(int i = 0; i < NUM_LEDS; i++)
+    {
+      leds[i].r = smooth_step(leds[i].r, leds_target_color[i].r, intermediate_colors_step_size);
+      leds[i].g = smooth_step(leds[i].g, leds_target_color[i].g, intermediate_colors_step_size);
+      leds[i].b = smooth_step(leds[i].b, leds_target_color[i].b, intermediate_colors_step_size);
+      //fill_read_buf(); // Empty the serial buffer and make sure we have the most current frame saved
+    }
+    FastLED.show();
+    //delay_microseconds_and_read(intermediate_colors_wait_time_micros);
+    delayMicroseconds(intermediate_colors_wait_time_micros);
+  }
+}
 
 void setup() {
   // Use NEOPIXEL to keep true colors
@@ -37,46 +138,42 @@ void setup() {
   Serial.print("Ada\n");
 }
 
+void read_ada_header()
+{
+  while(true)
+  {
+    for(i = 0; i < sizeof prefix; i++) 
+    {
+      // Check next byte in Magic Word
+      if(prefix[i] == read_byte()) continue;
+      // otherwise, start over
+      i = 0;
+    }
+    
+    // Hi, Lo, Checksum  
+    hi = read_byte();
+    lo = read_byte();
+    chk = read_byte();
+    
+    // If checksum does not match go back to wait
+    if (chk == (hi ^ lo ^ 0x55)) 
+    {
+      break;
+    }
+  }
+}
+
 void loop() { 
-  // Wait for first byte of Magic Word
-  for(i = 0; i < sizeof prefix; ++i) {
-    waitLoop: while (!Serial.available()) ;;
-    // Check next byte in Magic Word
-    if(prefix[i] == Serial.read()) continue;
-    // otherwise, start over
-    i = 0;
-    goto waitLoop;
-  }
-  
-  // Hi, Lo, Checksum  
-  while (!Serial.available()) ;;
-  hi=Serial.read();
-  while (!Serial.available()) ;;
-  lo=Serial.read();
-  while (!Serial.available()) ;;
-  chk=Serial.read();
-  
-  // If checksum does not match go back to wait
-  if (chk != (hi ^ lo ^ 0x55)) {
-    i=0;
-    goto waitLoop;
-  }
-  
-  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
+  read_ada_header();
+
   // Read the transmission data and set LED values
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
-    byte r, g, b;    
-    while(!Serial.available());
-    r = Serial.read();
-    while(!Serial.available());
-    g = Serial.read();
-    while(!Serial.available());
-    b = Serial.read();
-    leds[i].r = r;
-    leds[i].g = g;
-    leds[i].b = b;
+  for (uint8_t i = 0; i < NUM_LEDS; i++) 
+  {
+    leds_target_color[i].r = read_byte();
+    leds_target_color[i].g = read_byte();
+    leds_target_color[i].b = read_byte();
   }
-  
+
   // Shows new values
-  FastLED.show();
+  show_colors_smooth();
 }
--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 Adalight WS2812
 ===============
 
-This is a fork of Adalight working with WS2811/WS2812 LED using the FastLED library (v 3.1).
+This is a fork of Wifsimster/adalight_ws2812 and adds smooth color transitions.
 
-FastLED library can be found on Github here : https://github.com/FastLED/FastLED
+The following libraries are used:
+FastLED https://github.com/FastLED/FastLED
+CircularBuffer https://github.com/rlogiacco/CircularBuffer
+
+
+Notes
+===============
+The program is not in its finished state, yet.
+Currently, the prismatik frame time has to be set such that it's greater than the overall color transition time. Otherwise there will be artefacts.
+
+The total color transition time obviously contains `SMOOTH_TRANSITION_STEPS * SMOOTH_TRANSITION_WAIT_INTERMEDIATE`.
+Furthermore you have to include the time that `FastLED.show()` will take. According to https://github.com/FastLED/FastLED/wiki/Interrupt-problems you have to calculate 3ms per 100 LEDs. I personally have had better luck using an estimate of 6ms per 100 LEDs (3ms for 53 LEDs). So the formula that seems to work for my setup is: 
+```
+PrismatikFrametime >= (SMOOTH_TRANSITION_STEPS * SMOOTH_TRANSITION_WAIT_INTERMEDIATE) + (SMOOTH_TRANSITION_STEPS * ((NUM_LEDS/100) * 6ms))
+```
+
+Due to lack of more data this formula could be very wrong for different configurations.
+
+Any help is welcome!
